b6e7dc165e7e42643d82b56d1dbdeefaeb784a2c
Geuber <geuber@supersimplescred.com>
2022-02-27 17:13:03 -0300

    criação das paginas e da navbar

--- portifolio/src/app/app-routing.module.ts
+++ portifolio/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import {  NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculateComponent } from './calculate/calculate.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , component:HomeComponent},
+  {path: 'calculate' , component:CalculateComponent},
+  {path: 'projects' , component:ProjectsComponent},
+  {path: 'about', component:AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- portifolio/src/app/app.module.ts
+++ portifolio/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { CalculateComponent } from './calculate/calculate.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './sobre/sobre.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CalculateComponent,
+    ProjectsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
